d73f384764293ca1a3c0fba8bada1d59e69e542f
R-Bullen <raphaelbullen@gmail.com>
2025-05-11 05:19:43 +0000

    updated to new mzm name

--- dual_mzm_testing.py
+++ dual_mzm_testing.py
@@ -1,9 +1,9 @@
 import asp_sin_lnoi_photonics.all as asp
 import ipkiss3.all as i3
 
-from dual_mzm import DualMZM2x2
+from dual_mzm import DualMZM1x1
 
 
 if __name__ == '__main__':
-    dual_mzm = DualMZM2x2()
+    dual_mzm = DualMZM1x1()
     dual_mzm.Layout().visualize(annotate=True)
