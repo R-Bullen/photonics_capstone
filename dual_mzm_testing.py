08625d376dd65847d4a6dc55bf36426547196c5f
R-Bullen <raphaelbullen@gmail.com>
2025-05-17 14:56:16 +0000

    renamed to iq_modulator_design

--- dual_mzm_testing.py
+++
@@ -1,9 +0,0 @@
-import asp_sin_lnoi_photonics.all as asp
-import ipkiss3.all as i3
-
-from dual_mzm import DualMZM1x1
-
-
-if __name__ == '__main__':
-    dual_mzm = DualMZM1x1(bend_to_phase_shifter_dist=500, with_delays=True)
-    dual_mzm.Layout().visualize(annotate=True)
