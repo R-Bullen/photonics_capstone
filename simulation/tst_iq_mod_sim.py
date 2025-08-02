8c24929135540621a050ccff8620e0afa487f419
Administrator <s3760283@student.rmit.edu.au>
2025-08-02 14:03:30 +1000

    added sim iqmod2 modified test sim file

--- simulation/tst_iq_mod_sim.py
+++ simulation/tst_iq_mod_sim.py
@@ -1,14 +1,111 @@
+import asp_sin_lnoi_photonics.all as asp
 import numpy as np
 import matplotlib.pyplot as plt
 import ipkiss3.all as i3
 
 from components.iq_modulator_design import IQModulator
-from deprecated.tst_SimFunc import *
+#from deprecated.tst_SimFunc import *
+
+from sim_functions.simulate_iqmodulator import simulate_modulation_iqmod
 
 mod = IQModulator()
 lv = mod.Layout()
 #lv.visualize()
 
+cm = mod.CircuitModel()
+
+wavelengths = np.linspace(1.55, 1.555, 101)
+S = cm.get_smatrix(wavelengths=wavelengths)
+
+idx_min = np.argmin(np.abs(np.abs(S['out', 'in'])**2))
+idx_max = np.argmax(np.abs(np.abs(S['out', 'in'])**2))
+wl = wavelengths[int((idx_min + idx_max) / 2)]
+print("Quadrature wavelength: {}".format(wl))
+
+plt.figure()
+plt.plot(wavelengths * 1e3, np.abs(S['out', 'in'])**2)
+plt.plot([wl*1e3, wl*1e3], [0, 1])
+plt.plot(wavelengths * 1e3, [np.abs(S['out', 'in'][int((idx_min + idx_max) / 2)])**2 for x in wavelengths])
+plt.xlabel("Wavelength [nm]")
+plt.ylabel("Transmission [au]")
+plt.xlim([wavelengths[0] * 1e3, wavelengths[-1] * 1e3])
+plt.ylim(0, 1)
+plt.show()
+
+########################################################################################################################
+# Simulation of a MZM working in OOK modulation format.
+########################################################################################################################
+
+# Define modulator characteristics
+# NOTE - electrode length = 7000
+electrode_length = 7000
+rf_vpi = cm.vpi_l / 2 / (electrode_length / 10000)        # VpiL unit is V.cm; Dividing be 2 is due to push-pull configutation
+print("Modulator RF electrode Vpi: {} V".format(rf_vpi))
+
+cm.bandwidth = 50e9    # Modulator bandwidth (in Hz)
+
+results = simulate_modulation_mzm(
+    cell=mzm,
+    mod_amplitude=rf_vpi / 2 * 0.8,
+    mod_noise=0.01,
+    opt_amplitude=1.0,
+    opt_noise=0.01,
+    v_mzm1=0.0,
+    v_mzm2=0.0,
+    bit_rate=50e9,
+    n_bytes=2**8,
+    steps_per_bit=2**7,
+    center_wavelength=wl,
+)
+outputs = ["sig", "mzm1", "mzm2", "src_in", "out"]
+titles = [
+    "RF signal",
+    "Heater(bottom) electrical input",
+    "Heater(top) electrical input",
+    "Optical input",
+    "Optical output",
+]
+
+ylabels = ["voltage [V]", "voltage [V]", "voltage [V]", "amplitude [au]", "amplitude [au]"]
+process = [np.real, np.real, np.real, np.abs, np.abs]
+fig, axs = plt.subplots(nrows=len(outputs), ncols=1, figsize=(6, 10))
+for ax, pr, out, title, ylabel in zip(axs, process, outputs, titles, ylabels):
+    data = pr(results[out][1:])
+    ax.set_title(title)
+    ax.plot(results.timesteps[1:] * 1e9, data, label="mzm")
+    ax.set_xlabel("time [ns]")
+    ax.set_ylabel(ylabel)
+plt.tight_layout()
+
+########################################################################################################################
+# Plot EyeDiagram
+########################################################################################################################
+
+num_symbols = 2**8
+samples_per_symbol = 2**7
+data_stream = np.abs(results["out"]) ** 2
+baud_rate = 50e9
+time_step = 1.0 / (baud_rate * samples_per_symbol)
+eye = i3.EyeDiagram(data_stream, baud_rate, time_step, resampling_rate=2, n_eyes=2, offset=0.2)
+eye.visualize(show=False)
+
+########################################################################################################################
+# Plot Constellation diagram
+########################################################################################################################
+
+plt.figure(4)
+res = result_modified_OOK(results)
+plt.scatter(np.real(res), np.imag(res), marker="+", linewidths=10, alpha=0.1)
+plt.grid()
+plt.xlabel("real", fontsize=14)
+plt.ylabel("imag", fontsize=14)
+plt.title("Constellation diagram", fontsize=14)
+plt.xlim([-1.0, 1.0])
+plt.ylim([-1.0, 1.0])
+plt.show()
+
+
+'''
 test = i3.ConnectComponents(
     child_cells={
         "MOD": mod,
@@ -69,3 +166,4 @@ axs[1,1].legend()
 
 plt.tight_layout()
 plt.show()
+'''
