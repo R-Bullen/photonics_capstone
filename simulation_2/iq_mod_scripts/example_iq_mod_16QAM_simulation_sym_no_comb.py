3979c22fbba0b5cc6f33d4ee3754c93cf691093c
Administrator <s3760283@student.rmit.edu.au>
2025-09-27 19:45:02 +1000

    symmertic model no combined output

---
+++ simulation_2/iq_mod_scripts/example_iq_mod_16QAM_simulation_sym_no_comb.py
@@ -0,0 +1,28 @@
+
+"""Simulate an MZ modulator working in OOK modulation format. This is mainly based on Luceda Academy example.
+
+This script generates several outputs:
+- visualize the layout in a matplotlib window (this pauses script execution)
+- circuit simulation result (time-domain)
+- Plot EyeDiagram
+- Plot Constellation diagram
+"""
+
+import asp_sin_lnoi_photonics.all as asp
+import ipkiss3.all as i3
+from custom_components.iq_modulator_design_symmetric_model_no_combined_output import IQModulator
+from simulation_2.iq_mod_scripts.simulation.simulate_iq_mod_16QAM_no_combiner import simulate_modulation_16QAM, result_modified_16QAM
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+########################################################################################################################
+# Create a 1x1 MZM with a length difference between the two arms to set the biasing point 
+# by selecting the operating wavelength
+########################################################################################################################
+
+electrode_length = 8000
+iq_mod = IQModulator(with_delays=False, delay_at_input=True)
+
+lv = iq_mod.Layout(electrode_length=electrode_length, hot_width=50, electrode_gap=9)
+lv.visualize(annotate=True)
