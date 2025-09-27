6ea32bfc3a6209cc0e93b1b6d85c8596e2a5cae9
R-Bullen <raphaelbullen@gmail.com>
2025-09-27 19:11:33 +1000

    for visualising iq mod model

--- simulation_2/iq_mod_scripts/example_iq_mod_model.py
+++ simulation_2/iq_mod_scripts/example_iq_mod_model.py
@@ -19,7 +19,7 @@ from custom_components.iq_modulator_design_symmetric_model import IQModulator
 ########################################################################################################################
 
 electrode_length = 8000
-iq_mod = IQModulator(with_delays=True, delay_at_input=True)
+iq_mod = IQModulator(with_delays=False, delay_at_input=True)
 
 lv = iq_mod.Layout(electrode_length=electrode_length, hot_width=50, electrode_gap=9)
 
