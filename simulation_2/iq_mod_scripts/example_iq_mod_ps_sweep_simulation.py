a64ceb7b43689958f1a2cfbaa0fb9565ed731efa
R-Bullen <raphaelbullen@gmail.com>
2025-09-29 17:33:35 +1000

    created ps sweep simulations to find max operating voltage for ht_q

---
+++ simulation_2/iq_mod_scripts/example_iq_mod_ps_sweep_simulation.py
@@ -0,0 +1,170 @@
+
+"""Simulate an MZ modulator working in OOK modulation format. This is mainly based on Luceda Academy example.
+
+This script generates several outputs:
+- visualize the layout in a matplotlib window (this pauses script execution)
+- circuit simulation result (time-domain)
+- Plot EyeDiagram
+- Plot Constellation diagram
+"""
+
+import asp_sin_lnoi_photonics.all as asp
+import ipkiss3.all as i3
+from custom_components.iq_modulator_design_new_model import IQModulator
+from simulation_2.iq_mod_scripts.simulation.simulate_iq_mod_ps_sweep import simulate_modulation_ps_sweep
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+########################################################################################################################
+# Create a 1x1 MZM with a length difference between the two arms to set the biasing point 
+# by selecting the operating wavelength
+########################################################################################################################
+
+electrode_length = 8000
+iq_mod = IQModulator(with_delays=True, delay_at_input=True)
+
+lv = iq_mod.Layout(electrode_length=electrode_length, hot_width=50, electrode_gap=9)
+#
+lv.visualize(annotate=True)
+
+# lv.to_canvas()
+
+########################################################################################################################
+# Find the operating wavelength so that the modulator is operating at the quadrature biasing point
+########################################################################################################################
+
+cm = iq_mod.CircuitModel()
+
+wavelengths = np.linspace(1.549, 1.555, 101)
+S = cm.get_smatrix(wavelengths=wavelengths)
+
+idx_min = np.argmin(np.abs(np.abs(S['out', 'in'])**2))
+idx_max = np.argmax(np.abs(np.abs(S['out', 'in'])**2))
+wl = wavelengths[int((idx_min + idx_max) / 2)]
+print("Quadrature wavelength: {}".format(wl))
+
+plt.figure()
+plt.plot(wavelengths * 1e3, np.abs(S['out', 'in'])**2)
+plt.plot([wl*1e3, wl*1e3], [0, 1])
+plt.plot(wavelengths * 1e3, [np.abs(S['out', 'in'][int((idx_min + idx_max) / 2)])**2 for x in wavelengths])
+plt.xlabel("Wavelength [nm]")
+plt.ylabel("Transmission [au]")
+plt.xlim([wavelengths[0] * 1e3, wavelengths[-1] * 1e3])
+plt.ylim(0, 1)
+plt.show()
+
+# wavelengths = np.linspace(1.549, 1.555, 101)
+# S = cm.get_smatrix(wavelengths=wavelengths)
+#
+# idx_min = np.argmin(np.abs(np.abs(S['out', 'in'])**2))
+# idx_max = np.argmax(np.abs(np.abs(S['out', 'in'])**2))
+# wl = wavelengths[int((idx_min + idx_max) / 2)]
+# print("Quadrature wavelength: {}".format(wl))
+#
+# plt.figure()
+# plt.plot(wavelengths * 1e3, np.abs(S['out', 'in'])**2)
+# plt.plot([wl*1e3, wl*1e3], [0, 1])
+# plt.plot(wavelengths * 1e3, [np.abs(S['out', 'in'][int((idx_min + idx_max) / 2)])**2 for x in wavelengths])
+# plt.xlabel("Wavelength [nm]")
+# plt.ylabel("Transmission [au]")
+# plt.xlim([wavelengths[0] * 1e3, wavelengths[-1] * 1e3])
+# plt.ylim(0, 1)
+# plt.show()
+
+########################################################################################################################
+# Simulation of a MZM working in OOK modulation format.
+########################################################################################################################
+
+# Define modulator characteristics
+# Voltage needed for a pi phase shift and half pi shift
+rf_vpi = cm.vpi_l / 2 / (electrode_length / 10000)        # VpiL unit is V.cm; Dividing be 2 is due to push-pull configutation
+V_half_pi = rf_vpi / 2
+print("Modulator RF electrode Vpi: {} V".format(rf_vpi))
+
+ps_vpi = 0.1 / (200/10000)
+print("PS Vpi = %f" % ps_vpi)
+
+cm.bandwidth = 50e9    # Modulator bandwidth (in Hz)
+
+num_symbols = 2**10
+samples_per_symbol = 2**7
+bit_rate = 50e9
+
+results = simulate_modulation_ps_sweep(
+    cell=iq_mod,
+    mod_amplitude_i=rf_vpi/2,
+    mod_noise_i=0.0,
+    mod_amplitude_q=rf_vpi/2,
+    mod_noise_q=0.0,
+    opt_amplitude=2.0,
+    opt_noise=0.0,
+    v_heater_i=0, # The half pi phase shift implements orthogonal modulation
+    v_heater_q=ps_vpi/2,
+    v_mzm_left1=0,
+    v_mzm_left2=0,
+    v_mzm_right1=0,
+    v_mzm_right2=ps_vpi,
+    bit_rate=50e9,
+    n_bytes=num_symbols,
+    steps_per_bit=samples_per_symbol,
+    center_wavelength=1.55195,
+    qam_level=16,
+)
+
+# outputs = ["sig_i", "sig_q", "ht_q", "src_in", "out", "out"]
+outputs = ["ht_q", "out", "out"]
+titles = [
+    # "RF signal (top)",
+    # "RF signal (bottom)",
+    "Output PS voltage (output)",
+    # "Optical input",
+    "Optical output",
+    "Output Angle"
+
+    # "Optical output (bottom)",
+]
+# ylabels = ["voltage [V]", "voltage [V]", "voltage [V]", "amplitude [au]", "amplitude [au]", "angle"] #, "amplitude [au]", "amplitude [au]"]
+ylabels = ["voltage [V]", "amplitude [au]", "angle"] #, "amplitude [au]", "amplitude [au]"]
+# process = [np.real, np.real, np.real, np.abs, np.real, np.angle] #, np.real, np.real]
+process = [np.real, np.real, np.angle] #, np.real, np.real]
+
+fig, axs = plt.subplots(nrows=len(outputs), ncols=1, figsize=(6, 10))
+for ax, pr, out, title, ylabel in zip(axs, process, outputs, titles, ylabels):
+    data = pr(results[out][1:])
+    ax.set_title(title)
+    ax.plot(results.timesteps[1:] * 1e9, data, label="mzm")
+    ax.set_xlabel("time [ns]")
+    ax.set_ylabel(ylabel)
+plt.tight_layout()
+
+# index of max optical output
+max_index = np.argmax(np.real(results["out"]))
+print("max index:", max_index)
+max_wavelength = np.real(results["ht_q"][max_index])
+print("max power from voltage: {}".format(max_wavelength))
+
+########################################################################################################################
+# Plot EyeDiagram
+########################################################################################################################
+
+data_stream = np.abs(results["out"]) ** 2
+time_step = 1.0 / (bit_rate * samples_per_symbol)
+eye = i3.EyeDiagram(data_stream, bit_rate, time_step, resampling_rate=2, n_eyes=2, offset=0.2)
+eye.visualize(show=False)
+
+########################################################################################################################
+# Plot Constellation diagram
+########################################################################################################################
+
+# plt.figure(4)
+# res = result_modified_16QAM(results, samples_per_symbol=samples_per_symbol, sampling_point=0.8)
+# plt.scatter(np.real(res), np.imag(res), marker="+", linewidths=10, alpha=0.1)
+# plt.grid()
+# plt.xlabel("real", fontsize=14)
+# plt.ylabel("imag", fontsize=14)
+# plt.title("Constellation diagram", fontsize=14)
+# # plt.xlim([-1.0, 1.0])
+# # plt.ylim([-1.0, 1.0])
+plt.show()
