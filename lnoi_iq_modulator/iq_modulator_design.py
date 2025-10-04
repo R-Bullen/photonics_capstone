ea0e372cb5d7b7c102a4701beb898ef8095b8984
R-Bullen <raphaelbullen@gmail.com>
2025-10-04 14:42:04 +1000

    swapped ports on output_phase_shifter's

--- lnoi_iq_modulator/iq_modulator_design.py
+++ lnoi_iq_modulator/iq_modulator_design.py
@@ -896,11 +896,11 @@ class IQModulator(i3.PCell):
             ports += i3.expose_ports(self.instances, {'bottom_phase_shifter_2:m1': 'mzm_2_ps_2_in'})
             ports += i3.expose_ports(self.instances, {'bottom_phase_shifter_2:m2': 'mzm_2_ps_2_gnd'})
 
-            # output phase shifters
-            ports += i3.expose_ports(self.instances, {'top_output_phase_shifter:m1': 'mzm_1_ps_out_in'})
-            ports += i3.expose_ports(self.instances, {'top_output_phase_shifter:m2': 'mzm_1_ps_out_gnd'})
-            ports += i3.expose_ports(self.instances, {'bottom_output_phase_shifter:m1': 'mzm_2_ps_out_in'})
-            ports += i3.expose_ports(self.instances, {'bottom_output_phase_shifter:m2': 'mzm_2_ps_out_gnd'})
+            # output phase shifters, ports mirrored from input
+            ports += i3.expose_ports(self.instances, {'top_output_phase_shifter:m1': 'mzm_1_ps_out_gnd'})
+            ports += i3.expose_ports(self.instances, {'top_output_phase_shifter:m2': 'mzm_1_ps_out_in'})
+            ports += i3.expose_ports(self.instances, {'bottom_output_phase_shifter:m1': 'mzm_2_ps_out_gnd'})
+            ports += i3.expose_ports(self.instances, {'bottom_output_phase_shifter:m2': 'mzm_2_ps_out_in'})
 
             # phase modulator (dual mzm)
             ports += i3.expose_ports(self.instances, {'phase_modulator:top_ground': 'top_ground',
